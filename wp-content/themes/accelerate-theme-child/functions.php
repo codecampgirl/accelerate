8e85681383072fcdca1e882a06fc7b7326a0deab
mtrebo <mtrebo@me.com>
2017-01-16 13:24:46 +0100

    removed unnecessary sidebar code from functions.php and cleaned up CSS for sidebar

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -70,18 +70,3 @@ function accelerate_theme_child_widget_init() {
 }
 add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
 
-// add sidebar to pages //
-// Register widget area 
-function accelerate_theme_child_widgets_init() {
-	
-        register_sidebar( array(
-		'name'          => __( 'Sidebar 1', 'accelerate-theme-child' ),
-		'id'            => 'sidebar-1',
-		'description'   => __( 'Add widgets here to appear in your sidebar.', 'accelerate-theme-child' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h3 class="widget-title">',
-		'after_title'   => '</h3>',
-	) );
-}
-add_action( 'widgets_init', 'accelerate_theme_child_widgets_init' );
